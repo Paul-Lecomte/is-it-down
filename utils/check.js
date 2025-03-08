ad340061291915ced201cc1d47e014ff5a64ef1c
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-08 09:17:04 +0100

    added the logic to test the given url

--- utils/check.js
+++ utils/check.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
 export default async function(req, res) {
+    if (req.method !== "POST") return res.status(405).end();
 
+    const { url } = req.body;
+    if (!url) return res.status(400).json({ error: "URL is required" });
+
+    try {
+        const response = await axios.get(url, { timeout: 5000 });
+        res.json({ status: "up", code: response.status });
+    } catch (error) {
+        res.json({ status: "down", error: error.message });
+    }
 }
