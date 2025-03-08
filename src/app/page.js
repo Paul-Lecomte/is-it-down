be4ff13b5a99829bba6a23b817e1ee5b3d4e7149
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-08 09:24:40 +0100

    set states

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function Home() {
+  const [url, setUrl] = useState("");
+  const [result, setResult] = useState(null);
+
+
   return (
     <div></div>
   );
