fea806ae011eb3799bf4d6972da92b8cb5b20762
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-08 09:28:19 +0100

    more logiiiiiiiiiiiiiiiiiiiiiiic

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,15 @@ export default function Home() {
   const [url, setUrl] = useState("");
   const [result, setResult] = useState(null);
 
-
+  const checkWebsite = async () => {
+    if (!url) return;
+    try {
+      const { data } = await axios.post("../../utils/check.js", { url });
+      setResult(data);
+    } catch (err) {
+      setResult({ status: "down", error: err.message });
+    }
+  };
   return (
     <div></div>
   );
